f0e3c6fb443ac1f7f24f8a23f6980a8873192065
unknown <you@example.com>
2023-03-31 19:17:32 -0300

    setup gh-pages

--- src/component/break.jsx
+++ src/component/break.jsx
@@ -1,51 +1,49 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
-import { useStore } from '../context/provider' 
-import { actionType } from '../reducer/reducer'
-import './break.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useStore } from '../context/provider'; 
+import { actionType } from '../reducer/reducer';
+import './break.css';
 import Button from 'react-bootstrap/Button';
 
 const Break = () => {
-
-    const [{breakClock, loading}] = useStore();
-    const [state, dispatch] = useStore();
+    const [{ breakClock, loading }, dispatch] = useStore();
 
     const handleDecrement = () => {
         dispatch({
-            ...state,
             type: actionType.DECREASE_BREAK_CLOCK,
             breakClock: breakClock - 1
-        })
-    }
+        });
+    };
+
     const handleIncrement = () => {
         dispatch({
-            ...state,
             type: actionType.INCREASE_BREAK_CLOCK,
             breakClock: breakClock + 1
-        })
-    }
+        });
+    };
 
     return (
-        <div className='break'>
-            <span id='break-label' className='break-label'>Break Length</span>
+        <div className="break">
+            <span id="break-label" className="break-label">Break Length</span>
             <div className="break-controls">
                 <Button
-                    size='sm'
+                    size="sm"
                     variant="outline-primary"
-                    type='button'
-                    id='break-decrement'
-                    className='break-btn'
+                    type="button"
+                    id="break-decrement"
+                    className="break-btn"
                     onClick={handleDecrement}
-                    disabled={loading || breakClock <= 1}>
-                    <FontAwesomeIcon icon={faMinus}/>
+                    disabled={loading || breakClock <= 1}
+                >
+                    <FontAwesomeIcon icon={faMinus} />
                 </Button>
-                <p id="break-length" className='break-length'>{breakClock}</p>
+                <p id="break-length" className="break-length">{breakClock}</p>
                 <Button
-                    size='sm'
+                    size="sm"
                     variant="outline-primary"
-                    type='button'
-                    id='break-increment'
-                    className='break-btn'
+                    type="button"
+                    id="break-increment"
+                    className="break-btn"
                     onClick={handleIncrement}
                     disabled={loading || breakClock > 59}
                 >
@@ -53,7 +51,7 @@ const Break = () => {
                 </Button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Break;
